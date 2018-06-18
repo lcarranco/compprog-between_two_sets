79f4d27ca6779aeef9c587b3a6f25cd685ddf57a
lcarranco <lcarranco@gmail.com>
2018-06-18 11:15:42 -0500

    Set arrays A and B

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    int n = 0;
+    int m = 0;
+    int value = 0;
+    vector<int> A;
+    vector<int> B;
+
+    cin >> n;
+    // cout << n << endl;
+    cin >> m;
+    // cout << m << endl;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> value;
+        A.push_back(value);
+        // cout << A[i] << " ";
+    }
+    // cout << endl;
+
+    for (int i = 0; i < m; ++i) {
+        cin >> value;
+        B.push_back(value);
+        // cout << B[i] << " ";
+    }
+
     return 0;
 }
